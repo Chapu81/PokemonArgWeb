ebbb00ddc1edf84faad05543112a4eaf1770a18e
Ezequiel Guardia <eguardia@rava.com>
2021-05-25 03:50:46 -0300

    conectado a firebase

--- pokemonargweb/src/main.js
+++ pokemonargweb/src/main.js
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLn-42vaSeU41pW04o3j7x7tX_IAcNaKw",
+  authDomain: "pokeweb-23367.firebaseapp.com",
+  projectId: "pokeweb-23367",
+  storageBucket: "pokeweb-23367.appspot.com",
+  messagingSenderId: "535344882549",
+  appId: "1:535344882549:web:917fc051539d089f6fe93c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- pokemonargweb/src/views/About.vue
+++ pokemonargweb/src/views/About.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div>
+	<h1>This is an about page</h1>
+	<button @click="push_card">Push data</button>
+</div>
 </template>
+
+<script>
+import db from '../main'
+export default {
+	name: 'about',
+
+	data: () => ({
+
+	}),
+
+	mounted() {
+		this.get_cards();
+	},
+
+	methods: {
+		async get_cards() {
+			try {
+				const snapshot = await db.collection('cards').get();
+				// const cards = [];
+				snapshot.forEach(doc => {
+					console.log(doc.data());
+				});
+			}catch (error) {
+				console.log(error);
+			}
+		},
+		
+		async push_card() {
+			try {
+				await db.collection('cards').add({
+					img: 'po',
+					name: 'lala ',
+					price: '215212',
+					language: 'ingles',
+				});
+
+				this.get_cards();
+			}catch (error) {
+				console.log(error);
+			}
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
